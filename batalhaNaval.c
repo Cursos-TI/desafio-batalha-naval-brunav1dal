5118ff1e87ce1b6af3cf32137f69f293160230a3
brunav1dal <brunavidal2209@gmail.com>
2025-10-07 00:28:51 +0000

    Nivel novato, criação do tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,36 +5,53 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+//cria lista de cabeçalho
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    char letras[] = {'A', 'B', 'C', 'D', 'E' , 'F' ,'G','H', 'I', 'J'};
+    int tab[10][10];
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+// zerar a matriz toda para evitar buffer
+
+for (int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++){
+            tab[i][j] = 0;
+        }
+    }
+
+
+//imprime o cabeçalho com letras nas respectivas colunas.
+    printf("\n           BATALHA NAVAL\n");
+    printf("   ");
+    for (int j = 0; j < 10; j++) {
+        printf(" %c ", letras[j]);
+    }
+    printf("\n");
+
+
+//criar os dois navios, um na horizontal e outro na vertical.
+
+for ( int i = 3; i < 6; i++) //navio na vertical representado por 3
+{
+    tab[i][4] = 3;
+}
+
+for ( int j = 3; j < 6; j++) //navio na horizontal representado por 3
+{
+    tab[8][j] = 3;
+}
+
+//imprime o cabeçalho com letras nas respectivas linhas e tambem printa os elementos da matriz
+
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
+    for (int i = 0; i < 10; i++) {
+        printf(" %d ", i);
+        for(int j = 0; j < 10; j++){
+            printf(" %d ", tab[i][j]);
+        }
+        printf("\n");
+    }
+//ponto  interessante: a matriz tab[][] é preenchida com 0 e não faz parte da linha de cabeçalho de A a H e nem da coluna de 0 a 9
+   
     return 0;
 }
